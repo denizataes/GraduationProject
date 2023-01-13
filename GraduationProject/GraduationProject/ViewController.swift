88d6e00e2074031e63b786e0a3b9117594021dd0
Deniz Ata EŞ <85442526+denizataes@users.noreply.github.com>
2023-01-13 20:25:30 +0300

    Initial Commit
    Create Project

---
+++ GraduationProject/GraduationProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GraduationProject
+//
+//  Created by Deniz Ata Eş on 13.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
